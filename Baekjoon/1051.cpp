27c2f1c243c6bb2c2e40cda3cebc9afb6614044a
sangmu1126@gmail.com <sangmu1126@gmail.com>
2024-04-06 03:04:23 +0900

    240406

--- Baekjoon/1051.cpp
+++ Baekjoon/1051.cpp
@@ -1,19 +1,55 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <cmath>
+#include <array>
 using namespace std;
 
 int main() {
     int N,M;
     cin >> N >> M;
 
-    vector<int> rect;
+    array<vector<pair<int, int>>,10> nums;
+    vector<vector<int>> dx;
+    vector<vector<int>> dy;
 
     for (int i=0; i < N; i++) {
-        for(int j=0; j < N; j++) {
-            int k;
-            cin >> k;
+        string s;
+        cin >> s;
 
-            rect.push_back(k);
+        for (int j=0; j < s.size(); j++) {
+            int k = s[j]-'0';
+            nums[k].push_back(make_pair(i, j));
         }
     }
+    int ans = 1;
+
+    for (int i=0; i < 10; i++) {
+        cout << i << " : ";
+        for(int j=0; j < nums[i].size(); j++) {
+            cout << "(" << nums[i][j].first << nums[i][j].second << ")";
+        }
+        cout << endl;
+    }
+
+    int x;
+
+    for (int i=0; i < 10; i++) {
+        for (int j=0; j < nums[i].size(); j++) {
+            if (nums[i].size() > 1 && (nums[i][nums[i].size()-1].first - nums[i][0].first == nums[i][nums[i].size()-1].second - nums[i][0].second)) {
+                x = i;
+                cout << i << ": ";
+                cout << "(" << nums[i][j].first << ","<< nums[i][j].second << ")";
+                ans = pow(nums[i][nums[i].size()-1].first - nums[i][0].first + 1,2);
+            }
+        }
+    }
+
+    for (int i=0; i < x; i++) {
+        for (int j=i; j < x; j++) {
+            dx[i][j] = nums[x][j].first - nums[x][i].first;
+            dy[i][j] = nums[x][j].second - nums[x][i].second;
+        }
+    }
+    cout << ans;
 }
